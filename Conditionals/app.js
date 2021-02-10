d1d33a31eeeab4df48a73fbb1c5e7336eb619b72
ctdorris <ctdorris@gmail.com>
2021-02-10 15:07:26 -0600

    JavaScript

---
+++ Conditionals/app.js
@@ -0,0 +1,40 @@
+// let random = Math.random()
+// if (random < 0.5){
+//     console.log("YOUR NUMBER IS LESS THAN 0.5!!!")
+//     console.log(random);
+// }
+
+// if (random >= 0.5) {
+//     console.log("YOUR NUMBER IS GREATER THAN 0.5!!!")
+//     console.log(random)
+// }
+
+
+const dayOfWeek = 'Tuesday';
+
+if(dayOfWeek === 'Monday') {
+    console.log("UGHHH I HATE MONDAYS!")
+} else if (dayOfWeek ==='Saturday') {
+    console.log("YAY I LOVE SATURDAYS!")
+} else if (dayOfWeek ==='Friday') {
+    console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!")
+} else {
+    console.log("meh")
+}
+
+// 0 - 5 - FREE 
+// 5 - 10 CHILD $10
+// 10 - 65 ADULT $20
+// 65+ SENIOR $10
+
+// const age = 66;
+
+// if (age < 5) {
+//     console.log("You are a baby. You get in for free!")
+// } else if (age < 10) {
+//     console.log("You are a child. You pay $10")
+// } else if (age < 65) {
+//     console.log("You are an adult. You pay $20")
+// } else {
+//     console.log("You are a senior. You pay $10")
+// }
