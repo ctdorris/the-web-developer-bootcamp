95a5910e810f526b3cd8dd9149313cde07bf273c
ctdorris <ctdorris@gmail.com>
2021-09-07 09:26:50 -0500

    work 9/7

---
+++ MongooseBasics/index.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/movieApp')
+    .then(() => {
+        console.log("CONNECTION OPEN!")
+    })
+    .catch(err => {
+        console.log("OH NO ERROR!!!!!")
+        console.log(err)
+    })
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+const amadeus = new Movie({ title: 'Amamdeus', year: 1986, score: 9.2, rating: 'R' })
